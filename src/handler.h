d0d9a2a500b4cb3494b9e8c5e99f2e66853cbb7f
Wiktor <wikii122@gmail.com>
2014-05-29 11:43:54 +0200

    Some clean up, added debug flag to makefile

--- src/handler.h
+++ src/handler.h
@@ -5,16 +5,16 @@
 #include <string>
 
 // Feel free to change API
-class AbstractHandler
+class Handler
 {
-	int socket_fd;
-	void init();
 public:
-	AbstractHandler();
-	AbstractHandler(const AbstractHandler&) = delete;
-	~AbstractHandler();
+	Handler() = default;
+	Handler(const Handler&) = delete;
+	~Handler() = default;
 
 	virtual int handle();
 	virtual auto read() -> std::string = 0;
 	virtual int write(char* msg, int len) = 0;
+protected:
+	int socket_fd;
 };
