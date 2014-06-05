9bd99a469516aef5407bdbcc2641acb36bc1a3d8
Damian Rakowski <attil2@gmail.com>
2014-06-05 16:47:45 +0200

    Added a little of broadcast support

--- src/networkhandler.cpp
+++ src/networkhandler.cpp
@@ -1,5 +1,10 @@
 #include "networkhandler.h"
 
+#include <sys/socket.h>
+#include <netinet/in.h>  
+#include <arpa/inet.h> 
+#include <string.h>
+
 NetworkHandler::NetworkHandler()
 {
 }
@@ -21,4 +26,30 @@ auto NetworkHandler::read() -> std::string
 int NetworkHandler::write(std::string)
 {
 	return 0;
+}
+
+void NetworkHandler::createBroadcastSocket()
+{
+	sockaddr_in addr;
+	int sock;
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock == -1)
+		throw std::string("NetworkHandler::createBroadcastSocket: Could not create socket");
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(31337);
+	addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+	const bool isTrue = true;
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &isTrue, sizeof(isTrue)) == -1)
+		throw std::string("NetworkHandler::createBroadcastSocket: Could not setsockopt(BROADCAST)");
+
+	if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == -1)
+		throw std::string("NetworkHandler::createBroadcastSocket: Could not bind socket");
+
+	//TODO: REMOVE
+	if (sendto(sock, "COS", 4, 0, (sockaddr*) &addr, sizeof addr) == -1)
+		throw std::string("NetworkHandler::createBroadcastSocket: Could not sendto");
 }
