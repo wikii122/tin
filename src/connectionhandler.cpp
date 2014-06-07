60fbda835cb4c0b577e431e0b89147c043ff4999
Damian Rakowski <attil2@gmail.com>
2014-06-07 23:24:07 +0200

    Added ConnectionHandler

---
+++ src/connectionhandler.cpp
@@ -0,0 +1,34 @@
+#include "connectionhandler.h"
+
+#include <sys/socket.h>
+#include <iostream>
+#include <netinet/in.h>  
+#include <arpa/inet.h> 
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+
+#include "packet/packet.h"
+
+ConnectionHandler::ConnectionHandler()
+{
+}
+
+ConnectionHandler::~ConnectionHandler()
+{
+}
+
+int ConnectionHandler::handle()
+{
+}
+
+auto ConnectionHandler::read() -> std::string
+{
+}
+
+int ConnectionHandler::write(std::string msg)
+{
+}
---
+++ src/connectionhandler.h
@@ -0,0 +1,34 @@
+/*
+ * Module responsible for handling networking
+ */
+
+#ifndef CONNECTION_HANDLER_
+#define CONNECTION_HANDLER_
+#include "handler.h"
+#include "packet/packet.h"
+#include "packet/giveFileListPacket.h"
+#include "packet/iHavePacket.h"
+#include "packet/giveMePacket.h"
+#include "packet/iGotPacket.h"
+#include "packet/objectionPacket.h"
+#include "packet/forgetPacket.h"
+#include "packet/iForgotPacket.h"
+
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>  
+#include <arpa/inet.h> 
+#include <queue>
+#include <mutex>
+
+class ConnectionHandler: public Handler
+{
+public:
+	ConnectionHandler();
+	~ConnectionHandler();
+
+	virtual int handle() override; //Blocking!
+	virtual auto read() -> std::string override; // Blocking
+	virtual int write(std::string) override;
+};
+#endif
